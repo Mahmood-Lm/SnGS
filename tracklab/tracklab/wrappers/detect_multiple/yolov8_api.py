05d8cd76e745b54b54b4f9b1120ea321e3a1ee57
Mahmood <mahmood.lotfalizade@gmail.com>
2024-10-31 15:14:14 +0000

    add .gitignore and made changes to use trained yolov8 for detection

--- tracklab/tracklab/wrappers/detect_multiple/yolov8_api.py
+++ tracklab/tracklab/wrappers/detect_multiple/yolov8_api.py
@@ -57,8 +57,8 @@ class YOLOv8(ImageLevelModule):
             results_by_image, shapes, metadatas.iterrows()
         ):
             for bbox in results.boxes.cpu().numpy():
-                # check for `person` class
-                if bbox.cls == 0 and bbox.conf >= self.cfg.min_confidence:
+                # discard `ball` class
+                if bbox.cls != 0 and bbox.conf >= self.cfg.min_confidence:
                     detections.append(
                         pd.Series(
                             dict(
@@ -66,7 +66,7 @@ class YOLOv8(ImageLevelModule):
                                 bbox_ltwh=ltrb_to_ltwh(bbox.xyxy[0], shape),
                                 bbox_conf=bbox.conf[0],
                                 video_id=metadata.video_id,
-                                category_id=1,  # `person` class in posetrack
+                                category_id= bbox.cls,  # Classes: 1: 'goalkeeper', 2: 'player', 3: 'referee'
                             ),
                             name=self.id,
                         )
